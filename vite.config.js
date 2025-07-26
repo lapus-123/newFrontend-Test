d14484af535509fb02a49fe43a9c1faaeb880bcd
Grabby2024 <leornas.g20@gmail.com>
2025-07-26 23:04:57 +0800

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    historyApiFallback: true, // ⬅️ This enables proper routing fallback in dev
-  },
   build: {
-    outDir: 'dist', // or whatever your output dir is
+    outDir: 'dist'
   }
 })
